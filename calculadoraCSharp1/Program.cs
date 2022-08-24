285eda6dd049dbc92b9998c4650512c9e3909083
DominiqueSS <satsukiani@gmail.com>
2022-08-24 00:14:59 -0300

    Add project files.

---
+++ calculadoraCSharp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadoraCSharp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero1, numero2, resultado, menu;
+            float nDiv1, nDiv2,rDiv;
+
+            texto();
+            Console.WriteLine("\n\n\n\t\tClique em ENTER para continuar");
+
+
+            Console.ReadLine();
+
+
+
+
+             void texto()
+            {
+
+                Console.WriteLine("\t\t\t*****************");
+                Console.WriteLine("\t\t\t***Calculadora***");
+                Console.WriteLine("\t\t\t*****************");
+
+            }
+        }
+    }
+}
+
